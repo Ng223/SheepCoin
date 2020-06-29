f8826fdcc689d9a8ddb43f60c68aaf28a725165e
Nishant <nishantgavhane99@gmail.com>
2020-06-29 21:30:34 +0530

    made changes to truffle-config.js - for ropsten testnet.

--- truffle-config.js
+++ truffle-config.js
@@ -1,17 +1,19 @@
+var HDWalletProvider = require("@truffle/hdwallet-provider");
+const MNEMONIC = 'YOUR WALLET KEY';
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
   networks: {
     development: {
       host: "127.0.0.1",
-      port: "9545",
-      network_id: "5777" // match any network id
+      port: 9545,
+      network_id: "*"
     },
-    rinkeby: {
-      host: "localhost",
-      port: 8545,
-      network_id: 4,
-      gas: 4700000
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(MNEMONIC, "https://ropsten.infura.io/YOUR_API_KEY")
+      },
+      network_id: 3,
+      gas: 4000000      //make sure this gas allocation isn't over 4M, which is the max
     }
   }
 };
