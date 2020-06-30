e22831858538ca56731c6a103f124443a8ba3040
Nishant <nishantgavhane99@gmail.com>
2020-07-01 00:28:31 +0530

    increased the number of tokens

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,9 +2,9 @@ const SheepCoin = artifacts.require("./SheepCoin.sol");
 const SheepCoinSale = artifacts.require("./SheepCoinSale.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(SheepCoin,100000000).then(function() {
+  deployer.deploy(SheepCoin,10000000).then(function() {
     // Token price is 0.01 Ether
-    var tokenPrice = 1000000000000000;
+    var tokenPrice = 100000000000000;
     return deployer.deploy(SheepCoinSale, SheepCoin.address, tokenPrice);
   });
 };
